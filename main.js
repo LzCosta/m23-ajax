d44831e2d93e0ccedea20037e9748a9f49961978
LzCosta <116127270+LzCosta@users.noreply.github.com>
2024-10-20 06:30:57 -0300

    Commit fix: Correção no arquivo main.js
    Adição do "try/catch", para lidar com as exceções, caso ocorram na hora da requisição com a API.

--- main.js
+++ main.js
@@ -7,17 +7,23 @@ document.addEventListener("DOMContentLoaded", function () {
   const followingElement = document.querySelector("#following");
   const linkElement = document.querySelector("#link");
 
-  fetch(`https://api.github.com/users/LzCosta`)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (json) {
-      nameElement.innerText = json.name;
-      usernameElement.innerText = json.login;
-      avatarElement.src = json.avatar_url;
-      followingElement.innerText = json.following;
-      followersElement.innerText = json.followers;
-      reposElement.innerText = json.public_repos;
-      linkElement.href = json.html_url;
-    });
+  try {
+    fetch(`https://api.github.com/users/LzCosta`)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (json) {
+        nameElement.innerText = json.name;
+        usernameElement.innerText = json.login;
+        avatarElement.src = json.avatar_url;
+        followingElement.innerText = json.following;
+        followersElement.innerText = json.followers;
+        reposElement.innerText = json.public_repos;
+        linkElement.href = json.html_url;
+      });
+  } catch (erro) {
+    console.log("Ocorreu um erro ao requisitar a API.");
+  } finally {
+    console.log("Tudo certo com a requisição da API.");
+  }
 });
